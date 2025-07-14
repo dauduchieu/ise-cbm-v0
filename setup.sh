65c770a2b529152b02cd0a583f3b3de5da79478c
dauduchieu <dauduchieu.ddh775@gmail.com>
2025-07-14 21:22:52 +0700

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
-python3 -m venv venv
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11 python3.11-venv python3.11-dev
+
+python3.11 -m venv venv
+
+source venv/bin/activate
+
 pip install -r requirements.txt
