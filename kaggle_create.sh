2d6f78f802860c29ac4e9358825695b6a7bfbe18
dauduchieu <dauduchieu.ddh775@gmail.com>
2025-07-14 16:17:04 +0700

    lasted project

--- kaggle_create.sh
+++ kaggle_create.sh
@@ -1,4 +1,7 @@
 cp -f ./kaggle.json ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
 
+rm -rf venv
 kaggle datasets create -p . --dir-mode zip
+python3 -m venv venv
+pip install -r requirements.txt
--- kaggle_update.sh
+++ kaggle_update.sh
@@ -1,1 +1,4 @@
+rm -rf venv
 kaggle datasets version -p . --dir-mode zip -m "Update with subfolders"
+python3 -m venv venv
+pip install -r requirements.txt
